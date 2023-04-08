bc5bd9d2998bcbc8783d9d6f8e285848de22d9e6
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-04-08 17:18:52 +0600

    header updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
-import Navbar from './Components/Navbar/Navbar';
 import Shop from './Components/Shop/Shop';
+import Head from './Components/Head/Head';
 
 function App() {
   return (
     <div className="App bg-slate-50">
-      <Header/>
-      <Navbar/>
+      <Head/>
       <Routes>
         
         <Route path="/" element={<Home/>}></Route>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -84,8 +84,15 @@ const Home = () => {
                     >
                     <img src={product.image} alt="" className="object-cover object-center w-48 h-44 dark:bg-gray-500" />
                     
-                    <p className="dark:text-gray-100 text-start mx-2"> {product.name}</p>
-                    <h2 className="text-xl text-start font-semibold tracking-wide mx-2">${product.price}</h2>
+                    <p className="dark:text-gray-100 text-start mx-2"> {product.name.slice(0, 40)}..</p>
+                    <h2 className="text-m text-start text-red-500 font-semibold tracking-wide mx-2">${product.price}</h2>
+                    <h6 className="text-xs text-start text-gray-500  tracking-wide mx-2"> <span className='line-through'> $220  </span>  -32%</h6>
+                    <div className="reviews flex space-x-[1px] mb-3 p-2"><span><svg width="13" height="12" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FFA800"></path></svg></span><span><svg width="13" height="12" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FFA800"></path></svg></span><span><svg width="13" height="12" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FFA800"></path></svg></span>
+                    <span><svg width="13" height="12" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FFA800"></path></svg></span>
+                    <span><svg width="13" height="12" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z" fill="#FFA800"></path></svg></span>
+                    
+                    </div>
+                    
                 </div>                                      
                     ).slice(0,12)
                 }
