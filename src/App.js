d4aaf7b92ad6894e7b46670c9afa608eab64e589
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-04-11 22:02:49 +0600

    about added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './Components/Home/Home';
 import Shop from './Components/Shop/Shop';
 import Head from './Components/Head/Head';
 import PrivacyPolicy from './Components/Pages/PrivacyPolicy/PrivacyPolicy';
+import Contact from './Components/Pages/Contact/Contact';
+import Faq from './Components/Pages/Faq/Faq';
+import About from './Components/Pages/About/About';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/shop" element={<Shop/>}></Route>
         <Route path="/privacy-policy" element={<PrivacyPolicy/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/faq" element={<Faq/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
 
       </Routes>
       <Footer/>
--- src/Shared/Breadcumbs/Breadcumbs.js
+++ src/Shared/Breadcumbs/Breadcumbs.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Breadcumbs = () => {
+const Breadcumbs = (props) => {
     return (
         <div className="w-full mb-[30px]">
             <div className="page-title-wrapper bg-[#FFFAEF] w-full h-[173px] py-10">
                 <div className="container-x w-11/12 mx-auto">
                     <div className="mb-5">
-                        <div className="text-start breadcrumb-wrapper font-400 text-[13px] text-qblack mb-[23px]"><span><a href="/"><span className="mx-1 capitalize">home</span></a><span className="sperator">/</span></span><span><a href="privacy-policy"><span className="mx-1 capitalize">Privacy Policy</span></a><span className="sperator">/</span></span>
+                        <div className="text-start breadcrumb-wrapper font-400 text-[13px] text-qblack mb-[23px]"><span><Link to={"/"}><span className="mx-1 capitalize">home</span></Link><span className="sperator">/</span></span><span><Link to={props.path}><span className="mx-1 capitalize">{props.name}</span></Link><span className="sperator">/</span></span>
                         </div>
                     </div>
                     <div className="flex justify-center">
-                        <h1 className="text-3xl font-semibold text-qblack">Privacy Policy</h1>
+                        <h1 className="text-3xl font-semibold text-qblack">{props.name}</h1>
                     </div>
                 </div>
             </div>
